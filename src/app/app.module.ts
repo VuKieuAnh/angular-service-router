b8472fb50f2ed29cf38c5e1c0695ba0efbdf7faa
VuKieuAnh <vukieuanh.hnue@gmail.com>
2019-02-15 10:52:07 +0700

    dictionary-s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Injectable, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+
+// @Injectable()
+// export class DictionaryService {
+// }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DictionaryComponent
   ],
   imports: [
     BrowserModule,
